f6a9748008d69d527b443b7b721151bf9b281a00
xingjifengzheng <m15738519801@163.com>
2020-01-06 17:18:01 +0800

    no message

--- 22.js
+++ 22.js
@@ -1,14 +1,9 @@
 var c="大黄傻瓜"
 add 11111111111
-<<<<<<< HEAD
 const a=88888888888888888888
 let b=0000000000000000000
 const a=123455
 let b=7788899
-
-const a=123455
-let b=7788899
-const a=88888888888888888888
-let b=0000000000000000000
+uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu
 
 
